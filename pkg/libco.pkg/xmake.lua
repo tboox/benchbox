d9faed82bfad64695f11dc82cf122394541b7b0b
ruki <waruqi@gmail.com>
2016-09-02 18:25:30 +0800

    add libco case

---
+++ pkg/libco.pkg/xmake.lua
@@ -0,0 +1,28 @@
+-- add libco package
+option("libco")
+
+    -- show menu
+    set_showmenu(true)
+
+    -- set category
+    set_category("package")
+
+    -- set description
+    set_description("The libco package")
+
+    -- add defines to config.h if checking ok
+    add_defines_h_if_ok("$(prefix)_PACKAGE_HAVE_LIBCO")
+
+    -- add links for checking
+    add_links("co")
+
+    -- add link directories
+    add_linkdirs("lib/$(plat)/$(arch)")
+
+    -- add c includes for checking
+    add_cincludes("libco/libco.h")
+
+    -- add include directories
+    add_includedirs("inc/$(plat)", "inc")
+
+
---
+++ src/coroutine/switch/libco/xmake.lua
@@ -0,0 +1,15 @@
+-- enable libco?
+if is_option("libco") then
+
+    -- add target
+    target("coroutine_switch_libco")
+
+        -- set kind
+        set_kind("binary")
+
+        -- add files
+        add_files("*.c")
+
+        -- add package
+        add_packages("libco", "tbox", "base")
+end
