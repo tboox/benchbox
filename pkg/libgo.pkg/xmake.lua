cadc37bc6fa97c2c94c418255201424c8a995ab6
ruki <waruqi@gmail.com>
2016-10-21 09:21:29 +0800

    update libgo

--- pkg/libgo.pkg/xmake.lua
+++ pkg/libgo.pkg/xmake.lua
@@ -14,7 +14,7 @@ option("libgo")
     add_defines_h_if_ok("$(prefix)_PACKAGE_HAVE_LIBGO")
 
     -- add links for checking
-    add_links("go")
+    add_links("go", "boost_context")
 
     -- set languages
     set_languages("cxx11")
@@ -22,8 +22,8 @@ option("libgo")
     -- add link directories
     add_linkdirs("lib/$(plat)/$(arch)")
 
-    -- add c includes for checking
-    add_cxxincludes("libgo/libgo.h")
+    -- add c++ includes for checking
+    add_cxxincludes("libgo/libgo.h", "boost/coroutine/coroutine.hpp")
 
     -- add include directories
     add_includedirs("inc/$(plat)", "inc")
