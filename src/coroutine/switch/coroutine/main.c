2928a9ed1a6b48cee05245fd1d69e6b1729a35f3
ruki <waruqi@gmail.com>
2016-10-25 22:16:52 +0800

    modify demo trace

--- src/coroutine/switch/coroutine/main.c
+++ src/coroutine/switch/coroutine/main.c
@@ -74,7 +74,7 @@ tb_int_t main(tb_int_t argc, tb_char_t** argv)
         duration = tb_mclock() - duration;
 
         // trace
-        tb_trace_i("switch: coroutine: %d switches in %lld ms, %lld switches per second", COUNT, duration, (((tb_hong_t)1000 * COUNT) / duration));
+        tb_trace_i("switch: coroutine(cloudwu): %d switches in %lld ms, %lld switches per second", COUNT, duration, (((tb_hong_t)1000 * COUNT) / duration));
 
         // exit scheduler
         coroutine_close(scheduler);
